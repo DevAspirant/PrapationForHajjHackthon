9dba64863ef0f002d0bbc62e1522a61b2f688032
Falmban Ammar <devAspirant@gmail.com>
2018-07-25 15:45:47 +0300

    fix issues

--- oop.dart
+++ oop.dart
@@ -1,40 +1,36 @@
 // main function - الدالة الرئيسة
 void main(){
 //   create instance or object - عمل الاوبجيكت
-  var deck = new Deck();
-  
-  
+  new Deck(); // call the constructor - 
+
 }
 
-class Deck{
+class Deck {
 //   Adding Fileds -  
-  List<Card> cards;
+  List<Card> cards = []; // init the list - 
 // constructor -  الدالة الناشئة 
   Deck(){
     // defines - 
     var ranks = ['Ace','Two','Three','Four','Five'];
-    var suites = ['Diamonds','Hearts','Clubs','Spades'];
+    var suits = ['Diamonds','Hearts','Clubs','Spades'];
     
     // Loops that insert the all - 
-    for(var suit in suites){
+    for(var suit in suits){
       for(var rank in ranks){
         var card = new Card(rank, suit);
         cards.add(card); // add into the list - 
       }
     }
-    
   }
-  //   methods -
-  
+  //   methods -  
 }
 // define the Card class - 
 class Card{
 //   Adding fields 
   String suit;
   String rank;
+  
 //   constructor 
-  Card(){
-    
-  }
+  Card(this.rank, this.suit);
 //   methods 
 }
