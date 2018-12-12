31fa24f42ad78ad844f6f7e21849991ea3355e6d
Falmban Ammar <devAspirant@gmail.com>
2018-12-12 12:39:35 +0300

    Create Final_Const.dart

---
+++ Final_Const.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+// use final if you want a var/value to always be constant = never changes
+  
+final lastDeal = 1000; 
+  
+// lastDeal = 10; that is why there is an error
+  
+print(lastDeal); 
+  
+// use const when you want the value/var to be constant @ compiled time
+  
+const firstDeal = 10;
+
+	// firstDeal = 5; error  
+
+  print(firstDeal);
+  
+ }
+
