bf0932a06b003e39dcf071a63a946de8226ba45e
Falmban Ammar <devAspirant@gmail.com>
2018-12-12 12:45:27 +0300

    Create AccessToVar.dart

---
+++ AccessToVar.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+// my protofilio 
+  
+  String name = "Ammar"; 
+  
+  int age = 32; 
+  
+ print("My name is $name I,m $age");
+  
+ print("My name is ${name.toUpperCase()} I,m $age");
+  
+ }
+
